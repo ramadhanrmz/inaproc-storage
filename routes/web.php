8d532bacf4e91adfcaf440ed49ba4a858f0ed201
Aliwardana <mu.aliwardana@gmail.com>
2026-04-12 09:47:52 +0800

    edit routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\InaprocAccountController;
 
+/** 
 Route::get('/', function () {
     return redirect('/inaproc-accounts');
 });
@@ -10,3 +11,24 @@ Route::get('/', function () {
 Route::resource('/', InaprocAccountController::class)->names('inaproc-accounts');
 #Route::resource('inaproc-accounts', InaprocAccountController::class);
 Route::get('inaproc-export-pdf', [InaprocAccountController::class, 'exportPdf'])->name('inaproc.export-pdf');
+*/
+
+Route::prefix('inaproc-accounts')->group(function () {
+    
+    // Halaman Utama (Daftar Akun)
+    Route::get('/', [InaprocAccountController::class, 'index'])->name('inaproc-accounts.index');
+    
+    // Resource Route untuk Create, Edit, Delete, dll
+    Route::resource('accounts', InaprocAccountController::class)->names([
+        'index'   => 'inaproc-accounts.index',
+        'create'  => 'inaproc-accounts.create',
+        'store'   => 'inaproc-accounts.store',
+        'show'    => 'inaproc-accounts.show',
+        'edit'    => 'inaproc-accounts.edit',
+        'update'  => 'inaproc-accounts.update',
+        'destroy' => 'inaproc-accounts.destroy',
+    ]);
+
+    // Export Routes
+    Route::get('/export/pdf', [InaprocAccountController::class, 'exportPdf'])->name('inaproc.export-pdf');
+});
